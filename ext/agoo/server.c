2a2251ee200cf1faa6b3fced9d52f9be58d49d0b
Peter Ohler <peter@ohler.com>
2018-01-29 20:25:55 -0800

    working on windows 29

--- ext/agoo/server.c
+++ ext/agoo/server.c
@@ -8,13 +8,13 @@
 #include <netinet/tcp.h>
 #include <poll.h>
 #include <sys/select.h>
+#include <sys/socket.h>
 #endif
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/time.h>
 #include <unistd.h>
 
